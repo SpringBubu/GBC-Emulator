9b4c08f81a07ad0276444c9948c99a8904fc6081
Beni <bese2907@gmail.com>
2026-09-24 14:44:59 +0200

    added basic register struct

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,14 +1,41 @@
 enum RegisterName {
+    // 8-bit registers
+    A,
+    F,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
 
+    // 16-bit registers
+    AF,
+    BC,
+    DE,
+    HL,
+
+    SP,
+    PC,
+}
+
+enum FlagName {
+    Z,
+    N,
+    H,
+    C,
 }
 
 struct Flags {
-
+    zero: bool,
+    subtraction: bool,
+    half_carry: bool,
+    carry: bool,
 }
 
 struct Register {
     a: u8,
-    f: u8,
+    f: Flags,
     b: u8,
     c: u8,
     d: u8,
@@ -20,5 +47,23 @@ struct Register {
 }
 
 impl Register {
-
+    pub fn new(flag_state: bool) -> Self {
+        Self {
+            a: 0x01,
+            f: Flags {
+                zero: true,
+                subtraction: false,
+                half_carry: flag_state,
+                carry: flag_state,
+            },
+            b: 0x00,
+            c: 0x12,
+            d: 0x00,
+            e: 0xD8,
+            h: 0x01,
+            l: 0x4D,
+            sp: 0xFFEE,
+            pc: 0x0100,
+        }
+    }
 }
